27dbcf00d2367b880f7a68bf9af4273093e19433
吴伟斌 <wuwb@meitu.com>
2015-10-17 14:26:59 +0800

    [optimize] project initialize

---
+++ AiSandEmber/AiSandEmber/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AiSandEmber
+//
+//  Created by Webster Wu on 10/17/15.
+//  Copyright © 2015 Webster Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
