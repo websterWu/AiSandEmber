36f3ec1f8a4ff6969f937cd308502eebe84f59e4
吴伟斌 <wuwb@meitu.com>
2015-10-17 14:31:24 +0800

    【Optimize】新增ViewController

---
+++ AiSandEmber/AiSandEmber/ASEViewControllers/ASECenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  ASECenterViewController.h
+//  AiSandEmber
+//
+//  Created by Webster Wu on 10/17/15.
+//  Copyright © 2015 Webster Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASECenterViewController : UIViewController
+
+@end
---
+++ AiSandEmber/AiSandEmber/ASEViewControllers/ASECenterViewController.m
@@ -0,0 +1,37 @@
+//
+//  ASECenterViewController.m
+//  AiSandEmber
+//
+//  Created by Webster Wu on 10/17/15.
+//  Copyright © 2015 Webster Wu. All rights reserved.
+//
+
+#import "ASECenterViewController.h"
+
+@interface ASECenterViewController ()
+
+@end
+
+@implementation ASECenterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
